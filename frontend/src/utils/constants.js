8358ce0c1c090e3c95755ef50d19a8a16575faad
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-11-01 16:53:09 +0500

    fix: поправить пути на frontend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5555' : 'http://api.movement.nomoredomains.icu';
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5555' : 'https://api.movement.nomoredomains.icu';
 export default BASE_URL;
